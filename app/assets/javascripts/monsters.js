d13b9bd07b2b14a68a75451f1291cc533d34bfce
[jawdinmorris] <[jordanandrewmorris1@gmail.com]>
2018-04-09 17:33:20 +1000

    Added timer for battles.

---
+++ app/assets/javascripts/monsters.js
@@ -0,0 +1,31 @@
+// # Place all the behaviors and hooks related to the matching controller here.
+// # All this logic will automatically be available in application.js.
+// # You can use CoffeeScript in this file: http://coffeescript.org/
+var sec = 4;
+var myTimer = document.getElementById('myTimer');
+var myBtn = document.getElementById('myBtn');
+var battleScreen = document.getElementById('battleScreen');
+window.onload = countDown;
+
+function countDown() {
+  if (sec == 1) {
+    myTimer.innerHTML = "Currently fighting: " + "0" + sec + " second remains."
+        $("#myTimer").fadeTo(1000, 0);
+  }
+  else if (sec < 10) {
+    myTimer.innerHTML = "Currently fighting: " + "0" + sec + " seconds remain.";
+  } else {
+    myTimer.innerHTML = "Currently fighting: " + sec + " seconds remain.";
+  }
+  if (sec <= 0) {
+    $("#myBtn").removeAttr("disabled");
+    $("#myBtn").removeClass().addClass("btnEnable");
+
+    $("#battleScreen").fadeIn(1000, 1);
+
+    myBtn.value = "Fight Again!";
+    return;
+  }
+  sec -= 1;
+  window.setTimeout(countDown, 1000);
+}
